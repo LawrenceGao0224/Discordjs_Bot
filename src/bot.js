8b3eac7dc5d2caf68456cd2a4f00159c183e6753
LawrenceKao <bightt53523@gmail.com>
2021-05-03 21:34:37 +0800

    initial

---
+++ src/bot.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+
+const client = new Client();
+
+const PREFIX = "$";
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`);
+});
+
+client.on('message', (message) =>{
+    if (message.author.bot) return;
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        if(CMD_NAME === 'kick'){
+            if(args.length === 0) return message.reply('Please provide an ID!');
+            const member = message.guild.members.cache.get(args[0]);
+            if (member){
+                member.kick();
+            }else{
+                message.channel.send('That member was not found!')
+            }
+        }
+
+    }
+
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
